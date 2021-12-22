9febcaf7332e9b59b5f28a63c9ed20eea3ec9006
younes-alkashmi <93258554+younes-alkashmi@users.noreply.github.com>
2021-12-22 22:51:08 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+let userResult = 0;
+let compResult = 0;
+let words      = [];
+const userSpan = document.querySelector(".user-result");
+const compSpan = document.querySelector(".comp-result");
+const choices  = document.querySelectorAll(".choices > div");
+const resutlText = document.querySelector(".result-text");
+choices.forEach(function(element){
+    words.push(element.id);
+    element.addEventListener("click",function(el){
+        let compChoice = getRandom(words);
+        let userChoice = el.currentTarget.id;
+        
+        switch(userChoice + compChoice){
+            case"rockscissors":
+            case"scissorspaper":
+            case"paperrock":
+            userResult++;
+            userSpan.innerHTML = userResult;
+            resutlText.innerHTML = "You win 🔥🔥🔥";
+            break;
+            case"scissorsrock":
+            case"paperscissors":
+            case"rockpaper":
+            compResult++;
+            compSpan.innerHTML = compResult;
+            resutlText.innerHTML = "You lose . . .";
+            break;
+            default: resutlText.innerHTML = "It's a draw...";
+        }
+    })
+});
+
+function getRandom(arr){
+    return arr[Math.floor(Math.random()*arr.length)];
+}
